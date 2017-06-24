365f6e9cae96d10745e8471a438ae508d9a7f6d5
RJ Mohammad <rjmohammad@outlook.com>
2017-06-24 15:21:21 -0400

    added a remote method on a model

--- Loopback-Demo/common/models/product.js
+++ Loopback-Demo/common/models/product.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = function(Product) {
+  const validQuantity = quantity => quantity > 0;
+  /**
+ * Buy this Product
+ * @param {number} quantity Number of products to buy
+ * @param {Function(Error, object)} callback
+ */
 
+  Product.prototype.buy = function(quantity, callback) {
+    if (!validQuantity(quantity)){
+      return callback(`Invalid Quantity ${quantity}`);
+    }
+    var results = {
+      status: `you bought ${quantity} product`,
+    };
+    // TODO
+    callback(null, results);
+  };
 };
